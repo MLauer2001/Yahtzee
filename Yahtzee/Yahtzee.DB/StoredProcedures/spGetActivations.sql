3f005331bae6d844e5cb934202b123c3f17259fc
Matthew Lauer <300055427@fvtc.edu>
2022-04-07 10:12:47 -0500

    Added sp

---
+++ Yahtzee/Yahtzee.DB/StoredProcedures/spGetActivations.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spGetActivations]
+AS
+	SELECT a.Id,
+	a.LobbyId, a.Password, a.StartDate, a.EndDate
+	FROM tblActivation a
+	INNER JOIN tblLobby l on a.LobbyId = l.Id
+RETURN 0
