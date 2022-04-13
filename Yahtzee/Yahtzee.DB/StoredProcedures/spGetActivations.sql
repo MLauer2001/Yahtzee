5ba2551267166c4094eac781065e4a827fe6f27c
Riley <500172853@fvtc.edu>
2022-04-13 11:48:20 -0500

    Added PL project

--- Yahtzee/Yahtzee.DB/StoredProcedures/spGetActivations.sql
+++ Yahtzee/Yahtzee.DB/StoredProcedures/spGetActivations.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[spGetActivations]
 AS
 	SELECT a.Id,
-	a.LobbyId, a.Password, a.StartDate, a.EndDate
+	a.LobbyId, a.KeyCode, a.StartDate, a.EndDate
 	FROM tblActivation a
 	INNER JOIN tblLobby l on a.LobbyId = l.Id
 RETURN 0
---
+++ Yahtzee/Yahtzee.PL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Yahtzee.PL
+{
+    public class Class1
+    {
+    }
+}
