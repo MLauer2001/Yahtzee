c7cccd5d0118c93815b5d3b24d3c10cb21b52d44
Matthew Lauer <300055427@fvtc.edu>
2022-05-02 22:03:45 -0500

    Start on Web

---
+++ Yahtzee/Yahtzee.Web/Controllers/PlayerController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yahtzee.BL;
+using Yahtzee.BL.Models;
+using Yahztee.Web.Extensions;
+
+namespace Yahtzee.Web.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Login(string returnUrl)
+        {
+            TempData["returnurl"] = returnUrl;
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            SetUser(null);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            try
+            {
+                if (UserManager.Login(user))
+                {
+                    SetUser(user);
+                    if (TempData["returnurl"] != null)
+                        return Redirect(TempData["returnurl"].ToString());
+                    else
+                    {
+                        ViewBag.Message = "You are now logged in.";
+                    }
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(user);
+            }
+        }
+
+        private void SetUser(User user)
+        {
+            HttpContext.Session.SetObject("user", user);
+            if (user != null)
+            {
+                HttpContext.Session.SetObject("username", "Welcome " + user.Username);
+            }
+            else
+            {
+                HttpContext.Session.SetObject("username", "");
+            }
+        }
+
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Yahtzee/Yahtzee.Web/Models/Authenticate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yahtzee.BL.Models;
+using Yahztee.Web.Extensions;
+
+namespace Yahtzee.Web.Models
+{
+    public static class Authenticate
+    {
+        public static bool IsAuthenticated(HttpContext context)
+        {
+            if (context.Session.GetObject<User>("user") != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
