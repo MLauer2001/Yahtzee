61317a0a834e9215793ffd5221c5f46ec232d667
Matthew Lauer <300055427@fvtc.edu>
2022-05-11 01:35:49 -0500

    SignalR Lobbies working

--- Yahtzee/Yahztee.API/Hubs/GameHub.cs
+++ Yahtzee/Yahztee.API/Hubs/GameHub.cs
@@ -8,9 +8,19 @@ namespace Yahztee.API.Hubs
 {
     public class GameHub : Hub
     {
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToGroup(string group, string user, string message)
+        {
+            return Clients.Group(group).SendAsync("ReceiveMessage", user, message);
         }
     }
 }
